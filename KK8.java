5273e10800d861c40b61b59c14c29f004181d50f
Nidhicodes01 <136168928+Nidhicodes01@users.noreply.github.com>
2023-07-02 19:06:09 +0530

    Add files via upload

---
+++ KK8.java
@@ -0,0 +1,19 @@
+/*1. Class name should start from captital letter.
+  2. Class name and file name should be same.
+  3. public added before class name  
+ */
+import java.util.*;
+
+public class KK8{
+    public static void main(String args[]){
+
+        Scanner s=new Scanner(System.in);
+        //System.out.println(s.nextInt());
+        //System.out.println(s.next());
+        System.out.println(s.nextLine());
+
+
+
+    }
+
+}
