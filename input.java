e3e5257c07000c756138abe7cbaabc8fec7f905f
Nidhicodes01 <136168928+Nidhicodes01@users.noreply.github.com>
2023-07-02 21:46:49 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class input{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        int roll_number=input.nextInt();
+        System.out.println("Your roll number is:" + roll_number);
+    
+    }
+}
