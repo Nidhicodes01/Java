9fe9812f114d61d17f9cd11d71bf0029da121a82
Nidhicodes01 <136168928+Nidhicodes01@users.noreply.github.com>
2023-07-03 13:42:05 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,15 @@
+/*for and while loop in java */
+
+import java.util.*;
+public class loop{
+    public static void main(String args[]){
+        //int count=1;
+        /*while(count!=5){
+            System.out.println(count);
+            count++;
+        }*/
+        for(int count=1; count!=5;count++){
+            System.out.println(count);
+        }
+    }
+}
