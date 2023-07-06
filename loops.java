a15a72fe82bfa556771af94c444a463e18d18ba6
Nidhicodes01 <136168928+Nidhicodes01@users.noreply.github.com>
2023-07-06 20:10:11 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class loops{
+    public static void main(String args[]){
+        /*for(int i=1;i>6;i++){
+            System.out.println(i);
+        }*/
+
+        /*for(int num=1;num<5;num+=2){
+            System.out.println(num);
+        }*/
+        //print numbers from 1 to n
+    //     Scanner input=new Scanner(System.in);
+    //     int n=input.nextInt();
+    //     for(int i=1;i<=n;i+=1){
+    //         System.out.println("Hello world");
+    //     }
+    // }
+
+    // int i=1;
+    // while(i<6){
+    //     System.out.print(i);
+    //     i+=1;
+    // }
+    int n=1;
+    do{
+        System.out.println(n);
+        n+=1;
+    }while(n<=5);
+
+    int x=7;
+    do{
+        System.out.println("Hi");
+
+    }while(x>9);
+}
+}
